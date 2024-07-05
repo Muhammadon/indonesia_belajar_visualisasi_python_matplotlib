93732cc5c9b0a7f9ed227c274cbc8ded13b33877
muhammadon <muhammadon20m@gmail.com>
2024-07-05 10:02:57 +0700

    # 2   voll 2, hehe

---
+++ 8_plotting_tipe_data_categorical.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+names = ['group_a', 'group_b', 'group_c']
+values = [1, 10, 100]
+
+# plt.plot(names, values)
+# plt.show()
+
+# plt.scatter(names, values)
+# plt.show()
+
+plt.bar(names, values)
+plt.show()
